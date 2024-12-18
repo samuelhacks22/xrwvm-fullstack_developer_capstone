71076a2411ecf2659a915e1b415ea77ea6d17cf1
samuelhacks22 <samuelvasquezdiaz2@gmail.com>
2024-12-18 12:36:26 -0400

    Adding temporary changes to Github3

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -62,8 +62,10 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static')
+                        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -138,7 +140,9 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static')
+    os.path.join(BASE_DIR, 'frontend/static'),
+    os.path.join(BASE_DIR, 'frontend/build'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]# No code was selected, so we will add a new section to improve the code
 # Add a new section for logging configuration
 LOGGING = {
